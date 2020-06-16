ecb1247dca2cf8e1a6036beb5b2d104f472e1e3a
Matt Leuer <mattleuer@gmail.com>
2020-06-15 19:08:34 -0700

    Amount in input field is stored in state.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,34 @@ import Form from "./components/form";
 import Jumbotron from "./components/jumbotron";
 import Table from "./components/table";
 
-function App() {
-  return (
-    <div className="container">
-      <Jumbotron/>
-      <Form/>
-      <Table/>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    expenses: [],
+    amount: 0
+  }
+
+  handleInputChange = event => {
+    this.setState({...this.state, amount: event.target.value});
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    let expenseArr = this.state.expenses;
+    expenseArr.push(event.target.value);
+
+    this.setState({...this.state, expenses: expenseArr})
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <Jumbotron/>
+        <Form handleInputChange={this.handleInputChange} handleFormSubmit={this.handleFormSubmit}/>
+        <Table/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/form.js
+++ src/components/form.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Form() {
+function Form(props) {
   return (
     <>
-      <form>
+      <form onSubmit={props.handleFormSubmit}>
         <div className="form-group">
           <label>Input Your Expense:</label>
-          <input type="text" className="form-control" placeholder="$"></input>
+          <input type="text" className="form-control" onChange={props.handleInputChange} placeholder="$"></input>
         </div>
         <button type="submit" className="btn btn-primary">
           Submit
